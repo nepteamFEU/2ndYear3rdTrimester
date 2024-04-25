7ef49976640a5803ef5a32cfa70526987e92275e
nepteamFEU <202211274@feualabang.edu.ph>
2024-04-25 10:46:23 +0800

    adding TFA1
    deadline is tomorrow 11:59pm

---
+++ testinggrounds/index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title> Length Conversion Website</title>
+    </head>
+    <style>
+        body {
+            color: ;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        table, th, td
+        {
+           border: 1px solid;
+           border-color: black;
+        }
+    </style>
+    <body>
+        <table>
+            <tr>
+                <th> Metric Conversions </th>
+            </tr>
+        </table>
+    </body>
+</html>
