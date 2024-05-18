deaf667978b121f1ece98f3f34dd58e03892a8a3
nepteamFEU <202211274@feualabang.edu.ph>
2024-05-18 23:38:19 +0800

    finished 1, started another

---
+++ CCS0043/TSA2/FruitsArray.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title> Function Formulas </title>
+    </head>
+    <style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+        table, td, th
+        {
+           border: 1px solid;
+           border-color: black;
+           padding: 2px;
+        }
+    </style>
+    <body>
+    <table>
+            <tr>
+                <th colspan="4"> <h1> MY FRUITS </h1> </th>
+            </tr>
+            <tr>
+                <td><h2>IMAGE</h2></td>
+                <td><h2>NAME</h2></td>
+                <td><h2>DESCRIPTION</h2></td>
+                <td><h2>FACTS</h2></td>
+            </tr>
+    </body>
+</html>
--- CCS0043/TSA2/FunctionFormula.php
+++ CCS0043/TSA2/FunctionFormula.php
@@ -11,6 +11,7 @@
            border: 1px solid;
            border-color: black;
            text-align: center;
+           padding: 4px;
         }
     </style>
     <body>
@@ -49,6 +50,50 @@
                 echo "<td> l=$rrl<br>w=$rrw<br>h=$rrh  </td>";
                 echo "<td> V = lwh </td>";
                 echo "<td> $rightPrism </td>";
+                ?>
+            </tr>
+            <tr>
+                <?php
+                function prism($B,$H)
+                {
+                    return $B*$H;
+                }
+                $prismBase = 2;
+                $prismHeight = 6;
+                $prismVolume = prism($prismBase,$prismHeight);
+                echo "<td> b=$prismBase h=$prismHeight</td>";
+                echo "<td> V = bh </td>";
+                echo "<td> $prismVolume";
+                ?>
+            </tr>
+            <tr>
+                <?php
+                function pyramid($L,$W,$H)
+                {
+                    return ($L*$W*$H)/3;
+                }
+                $pyramidL = 23;
+                $pyramidW = 12;
+                $pyramidH = 6;
+                $pyramidVolume = pyramid($pyramidL,$pyramidW,$pyramidH);
+                echo "<td> l=$pyramidL<br>w=$pyramidW<br>h=$pyramidH </td>";
+                echo "<td> V = (lwh)/3 </td>";
+                echo "<td> $pyramidVolume </td>";
+                ?>
+            </tr>
+            <tr>
+                
+                <?php
+                function sphere($R)
+                {
+                    $RCUBE = $R*$R*$R;
+                    return ((4/3)*3.1415*($RCUBE));
+                }
+                $sphereR = 6.0;
+                $sphereVolume = sphere($sphereR);
+                echo "<td> r = $sphereR  </td>";
+                echo "<td> V = (4/3)πr<sup>3</sup> </td>";
+                echo "<td> $sphereVolume </td>";
                 ?>
             </tr>
         </table>
